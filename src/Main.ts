d308cf353ee587945d11a7734df703211c641040
Danny Feldhaus <danny.b.feldhaus@gmail.com>
2024-05-16 03:07:45 +0000

    Removed exmaple rooms

--- src/Main.ts
+++ src/Main.ts
@@ -21,7 +21,7 @@ class PlayerStatus {
 }
 
 /**
- * Abstract base class for room objects.
+ * Base class for room objects.
  */
 abstract class RoomObj {
   abstract room: Room;
@@ -49,96 +49,6 @@ abstract class RoomObj {
     status.room = this.room;
     let command = PromptNonEmpty("Please enter a command.");
     return this.process_command(command, status);
-  }
-}
-
-class RoomA extends RoomObj {
-  room: Room = "A";
-  describe(status: PlayerStatus): void {
-    if(status.room != this.room) {
-      console.info("You are in an empty room. There are doors on the north and west walls of this room.");
-    }
-  }
-
-  process_command(command: string, status: PlayerStatus): Room {
-    switch (command) {
-      case "west":
-        return "B";
-      case "north":
-        if (status.hasKey) {
-          console.info("You unlock the north door with the key and go through the door.");
-          return "C";
-        } else {
-          console.error("You try to open the north door, but it is locked.");
-          return "A";
-        }
-      default:
-        console.error("Unrecognized command.");
-        return "A";
-    }
-  }
-}
-
-class RoomB extends RoomObj {
-  room: Room = "B";
-  describe(status: PlayerStatus): void {
-    if (status.room != this.room) {
-      console.info("You go through the west door. You are in a room with a table.");
-      if (!status.hasKey) {
-        console.info("On the table there is a key.");
-      }
-      console.info("There is a door on the east wall of this room.");
-    }
-  }
-  process_command(command: string, status: PlayerStatus): Room {
-    switch (command) {
-      case "east":
-        return "A";
-      case "take key":
-        if (status.hasKey) {
-          console.error("You already have the key.");
-        } else {
-          console.info("You take the key from the table.");
-          status.hasKey = true;
-        }
-        return "B";
-      default:
-        console.error("Unrecognized command.");
-        return "B";
-    }
-  }
-}
-
-class RoomC extends RoomObj {
-  room: Room = "C";
-  describe(status: PlayerStatus): void {
-    if (status.room != this.room) {
-      console.info("You are in a bright room. There is a door on the south wall of this room and a window on the east wall.");
-    }
-  }
-  process_command(command: string, status: PlayerStatus): Room {
-    switch (command) {
-      case "south":
-        return "A";
-      case "east":
-        if (status.windowOpen) {
-          console.info("You step out from the open window.");
-          return "Exit";
-        }
-        console.error("The window is closed.");
-        return "C";
-      case "open window":
-        if (status.windowOpen) {
-          console.error("The window is already open.");
-        } else {
-          console.info("You open the window.");
-          status.windowOpen = true;
-        }
-        return "C";
-      default:
-        console.error("Unrecognized command.");
-        return "C";
-    }
   }
 }
 
